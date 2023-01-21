5e135cbaa6bbc17e38dee5cb745d145c2b3620d0
bjsapkota1313 <90284672+bjsapkota1313@users.noreply.github.com>
2023-01-21 11:09:23 +0100

    Lastly It Works

--- sql/OurMarketDB.sql
+++ sql/OurMarketDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Generation Time: Jan 20, 2023 at 10:04 PM
+-- Generation Time: Jan 21, 2023 at 10:02 AM
 -- Server version: 10.9.4-MariaDB-1:10.9.4+maria~ubu2204
 -- PHP Version: 8.0.25
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `OurMarketDB`
 --
+CREATE DATABASE IF NOT EXISTS `OurMarketDB` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `OurMarketDB`;
 
 -- --------------------------------------------------------
 
